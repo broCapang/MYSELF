6b1e5444565a7def67c1c62daf13c27aed334bbe
shafiqps <shafiqaimanps@gmail.com>
2021-12-11 22:50:40 +0800

    fuck u cpg

--- exercises/temp.java
+++ exercises/temp.java
@@ -2,6 +2,6 @@ package exercises;
 
 public class temp {
     public static void main(String[] args) {
-        System.out.println("Shafiq babi");
+        System.out.println("cpg funny voice effect");
     }
 }
