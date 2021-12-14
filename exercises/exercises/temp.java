99c738f09b7619af895648f4ecb2f4d109d065bc
CAPANG JABBA <ihaziq18.ih@gmail.com>
2021-12-14 11:25:08 +0800

    SOME ADJUSTMENTS

--- exercises/exercises/temp.java
+++ exercises/exercises/temp.java
@@ -4,71 +4,30 @@ import java.util.Scanner;
 
 public class temp {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner in=new Scanner(System.in);
 
-        // VARIABLES
-        String[] userEmail = new String[20];
-        int[] userPass = new int[20];
-        String[] adminEmail = new String[20];
-        int[] adminPass = new int[20];
+        System.out.print("Enter number of rows:");
+        int n=in.nextInt();
 
-        // TEST EMAIL AND PASS
-        userEmail[0] = "irfan";
-        userPass[0] = 000000;
-        adminEmail[0] = "shazwy";
-        adminPass[0] = 111111;
+        int[][] pascal=new int[n][n];
 
-        while (true){
-            // GET INPUT FROM USER
-            System.out.println("USER 1");
-            System.out.println("ADMIN 2");
-            System.out.print("Enter the code : ");
-            int code = input.nextInt();
-
-            System.out.print("Email: ");
-            String emailInput = input.next();
-            System.out.print("Password: ");
-            int passInput = input.nextInt();
-
-            // VERIFYING PHASE
-
-            switch (code){
-                case 1:
-                    //USER
-                    for (int i=0;i<20;i++){     //VERIFYING PHASE
-                        if (userEmail[0].equals(emailInput)){
-                            if (userPass[0]==passInput){
-                                // ENTER MOVIE PAGE
-                                System.out.println("WE IN MOVIE PAGE");
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                case 2:
-                    // ADMIN
-                    for (int i=0;i<20;i++){     //VERIFYING PHASE
-                        if (adminEmail[i].equals(emailInput)){
-                            if (adminPass[i]==passInput){
-                                // ENTER ADMIN PAGE
-                                System.out.println("WE IN ADMIN PAGE");
-                                break;
-                            }
-                        }
-                    }
-                    break;
-                default:
-                    // USER ENTER WRONG CODE
-                    System.out.print("Invalid code");
-                    break;
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (j==0||j==i){
+                    pascal[i][j]=1;
+                    continue;
+                }else if (j>i){
+                    pascal[i][j]=0;
+                    continue;
+                }
+                pascal[i][j]=pascal[i-1][j-1]+pascal[i-1][j];
             }
-
-            // ASK USER IF THEY STILL WANT TO USE THIS PROGRAM
-            System.out.print("CONTINUE? (Y/N) : ");
-            char lastQ = input.next().charAt(0);
-            if (lastQ == 'N'){
-                break;
+        }
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                System.out.print(pascal[i][j]+" ");
             }
+            System.out.println();
         }
     }
 }
