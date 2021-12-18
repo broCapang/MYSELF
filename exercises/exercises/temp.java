a4842438337a21d8629fe49868eb044b36446b2b
CAPANG JABBA <ihaziq18.ih@gmail.com>
2021-12-18 23:23:55 +0800

    lab 6 q 6 in temp

--- exercises/exercises/temp.java
+++ exercises/exercises/temp.java
@@ -4,8 +4,62 @@ import java.util.Scanner;
 
 public class temp {
     public static void main(String[] args) {
-        Scanner in=new Scanner(System.in);
-        int[] bruh = new int[5];
-        System.out.println(bruh.length);
+        int n = 0;
+        // 2, 3, 5, 7, 11, 101, 131, 151, 181, 191, 313, 353, 373, 383, 727, 757, 787, 797, 919, 929
+        // pC == palindromicCount
+        System.out.println("first 20 PALINDROME PRIMES");
+        for (int pC  = 0; pC  < 20;      ) {
+            if (isPalindromicPrime(n)){
+                pC++;
+                System.out.println(n);
+            }
+            n++;
+        }
+        System.out.println("\n\n\n");
+        n = 0;
+        // 13, 17, 31, 37, 71, 73, 79, 97, 107, 113, 149, 157, 167, 179, 199, 311, 337, 347, 359, 389,
+        // eC == emirpCount
+        System.out.println("first 20 ERIMP");
+        for (int eC = 0; eC < 20;      ) {
+            if (isEmirp(n)){
+                eC++;
+                System.out.println(n);
+            }
+            n++;
+        }
+    }
+
+
+
+    private static boolean isPalindromicPrime( int n) {
+        // not a palindrome number
+        if (n != reverse(n))
+            return false;
+
+        return isPrime(n);
+    }
+    private static int reverse(int n) {
+        int sum = 0;
+
+        while (n != 0){
+            sum = (sum*10) + (n%10);
+            n   /= 10;
+        }
+
+        return sum;
+    }
+    private static boolean isPrime( int n){
+        if (n < 2)
+            return false;
+        if (n == 2)
+            return true;
+
+        for (int i = 2; i <= Math.sqrt(n); i++)
+            if (n%i == 0)
+                return false;
+        return true;
+    }
+    private static boolean isEmirp( int n) {
+        return isPrime(n) && isPrime(reverse(n)) && !isPalindromicPrime(n);
     }
 }
