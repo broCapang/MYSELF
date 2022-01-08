34e9f08a8e9d30eb1aecb3bfb73ce391748289a7
CAPANG JABBA <ihaziq18.ih@gmail.com>
2022-01-08 20:29:30 +0800

    rectangle

--- exercises/exercises/lab9/Rectangle.java
+++ exercises/exercises/lab9/Rectangle.java
@@ -1,5 +1,21 @@
 package exercises.lab9;
 
 public class Rectangle extends Shape{
+    private double side1, side2;
+
+    public void setRectangle(String name, double perimeter, double area, double side1, double side2){
+        super.setShape(name,perimeter,area);
+        this.side1=side1;
+        this.side2=side2;
+
+    }
+
+    public double getPerimeter(double side1, double side2){
+        return side1*2.0+side2*2.0;
+    }
+
+    public double getArea(double side1, double side2){
+        return side1*side2;
+    }
 
 }
