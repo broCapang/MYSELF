bf91db3c23a81d5d3ad7b90bd4ffd8d32219ab8c
CAPANG JABBA <ihaziq18.ih@gmail.com>
2022-01-23 18:51:10 +0800

    test

---
+++ exercises/exercises/lab6/lab6q1.java
@@ -0,0 +1,24 @@
+package exercises.lab6;
+
+public class lab6q1 {
+    public static void main(String[] args) {
+        System.out.println("List of triangular numbers");
+        for (int i = 1; i < 20; i++) {
+            int tn = triangularNumber(i);
+            System.out.printf("%d) %d\n", (i+1), tn);
+        }
+    }
+
+    private static int triangularNumber( int n) {
+        int sum;
+        if (n==1){
+            sum = 1;
+            return sum;
+        }else {
+            sum=0;
+            for (int i = 1; i <= n; i++)
+                sum += i;
+            return sum;
+        }
+    }
+}
